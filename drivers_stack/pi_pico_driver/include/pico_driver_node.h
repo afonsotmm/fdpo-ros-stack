5a985b5dceb915c75e025fe6577ed1ab0b73a2ad
kane <kane@Ubuntu.myguest.virtualbox.org>
2025-10-03 19:35:38 +0100

    development of pi_pico_driver

--- drivers_stack/pi_pico_driver/include/pico_driver_node.h
+++ drivers_stack/pi_pico_driver/include/pico_driver_node.h
@@ -1,6 +1,16 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h>
+#include <std_msgs/Float32.h>
+
+struct Pose {
+
+    double x, y, theta;
+
+};
 
 class PiPicoDriver {
     public:
@@ -10,5 +20,17 @@ class PiPicoDriver {
         ros::NodeHandle& nh;
     
         ros::Subscriber velSub;
-        void velCallBack(const geometry_msgs::Twist& msg);
+        ros::Subscriber pickBoxSub;
+        ros::Publisher posePub;
+        ros::Publisher tofPub;
+        void PickBoxCallBack(const std_msgs::Bool::ConstPtr& msg);
+        void velCallBack(const geometry_msgs::Twist::ConstPtr& msg);
+        void PubPose();
+        void PubTof();
+
+        Pose position;
+        float Tof;
+        bool iman;
+        double v_d, w_d;
+
 };
--- drivers_stack/pi_pico_driver/src/pico_driver_node.cpp
+++ drivers_stack/pi_pico_driver/src/pico_driver_node.cpp
@@ -1,5 +1,55 @@
 #include "pico_driver_node.h"
 
-PiPicoDriver::PiPicoDriver(ros::NodeHandle& nh_) {
+
+PiPicoDriver::PiPicoDriver(ros::NodeHandle& nh_) : nh(nh_) {
     velSub = nh.subscribe("/cmd_vel", 10, &PiPicoDriver::velCallBack, this);
+    pickBoxSub = nh.subscribe("/pick_box", 10, &PiPicoDriver::PickBoxCallBack, this);
+    posePub = nh.advertise<geometry_msgs::Pose2D>("/odom", 10);
+    tofPub = nh.advertise<std_msgs::Float32>("/tof_measure", 10);
+    
+}
+
+
+void PiPicoDriver::PickBoxCallBack(const std_msgs::Bool::ConstPtr& msg) {
+    ROS_INFO_STREAM("Received pick_box msg: " << msg->data);
+
+    iman = msg->data;
+}
+
+void PiPicoDriver::velCallBack(const geometry_msgs::Twist::ConstPtr& msg) {
+    ROS_INFO_STREAM("Received cmd_vel: linear=" << msg->linear.x << ", angular=" << msg->angular.z);
+
+    v_d = msg->linear.x;
+    w_d = msg->angular.z;
+
+}
+
+void PiPicoDriver::PubPose(){
+    
+    geometry_msgs::Pose2D XYTheta;
+    XYTheta.x = position.x;
+    XYTheta.y = position.y;
+    XYTheta.theta = position.theta;
+    posePub.publish(XYTheta);
+
+}
+
+void PiPicoDriver::PubTof(){
+    
+    std_msgs::Float32 tofValue;
+    tofValue.data = Tof;
+    tofPub.publish(tofValue);
+    
+}
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "pico_driver_node");
+  ros::NodeHandle nh;
+
+  PiPicoDriver driver(nh);  // cria seu objeto controlador
+
+  ros::spin();  // espera callbacks
+
+  return 0;
 }
